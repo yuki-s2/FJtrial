f03fdcba35e48a0271fdaf8e8b710e6581564dae
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-04 05:42:06 +0800

    レスポンシブ追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-// 'use strict';
+'use strict';
 
 
 $(document).ready(function(){
 	if (window.matchMedia('(min-width: 768px)').matches) {
 
 $('.slider').slick({
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    speed: 500,//スライドのスピード。初期値は300。
-    slidesToShow: 3,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    centerMode: true,//要素を中央ぞろえにする
-    variableWidth: true,//幅の違う画像の高さを揃えて表示
-    dots: false,//下部ドットナビゲーションの表示
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    centerMode: true,
+    variableWidth: true,
+    dots: false,
 });
 
 }
